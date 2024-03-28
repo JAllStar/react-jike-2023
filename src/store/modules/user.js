14f469260acb492e20570a656da76da0ea9d6a37
Jinyang Huang <jeremy142857@gmail.com>
2024-03-28 10:01:41 -0700

    封装token小办法

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,16 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { request } from '@/utils'
+import { setToken as _setToken, getToken} from '@/utils'
 
 const userStore = createSlice({
   name: 'user',
   initialState: {
-    token: ''
+    token: getToken() || ''
   },
   reducers: {
     setToken (state, action) {
       state.token = action.payload
+
+      _setToken(action.payload)
     }
-  }
+  } 
 })
 
 const { setToken } = userStore.actions
